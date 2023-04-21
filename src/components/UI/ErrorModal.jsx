897ca5acfbac9cbb6d5240b1d0b8384185876154
IkramKhan <ikram5426@gmail.com>
2023-04-22 01:41:02 +0500

    React Portal added for error modal and erro backdrop

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import Button from "./Button";
 import Card from "./Card";
 import style from "./errorModal.module.css";
-
+import ReactDOM from "react-dom"
 const ErrorModal = (props) => {
   return (
     <React.Fragment>
-      <div className={style.backdrop} onClick={props.onConfirm}></div>
-      <Card style={style.modal}>
-        <header className={style.header}>
-          <h1>{props.title}</h1>
-        </header>
-        <div className={style.content}>{props.message}</div>
-        <footer className={style.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <>
+          <div className={style.backdrop} onClick={props.onConfirm}></div>
+          <Card style={style.modal}>
+            <header className={style.header}>
+              <h1>{props.title}</h1>
+            </header>
+            <div className={style.content}>{props.message}</div>
+            <footer className={style.actions}>
+              <Button onClick={props.onConfirm}>Okay</Button>
+            </footer>
+          </Card>
+        </>,document.getElementById('modal')
+      )}
     </React.Fragment>
   );
 };
