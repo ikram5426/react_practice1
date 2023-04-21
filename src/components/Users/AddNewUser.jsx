1c689d7ca79de0055403b07f59dd2986b2f9cb13
IkramKhan <ikram5426@gmail.com>
2023-04-22 01:59:18 +0500

    Added useRef instead of useState for getting user value

--- src/components/Users/AddNewUser.jsx
+++ src/components/Users/AddNewUser.jsx
@@ -1,66 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import style from "./addNewUser.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 
 const AddNewUser = (props) => {
-  const [enteredName, setEnteredName] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
-  const [error,setError]=useState('')
-
+  // const [enteredName, setEnteredName] = useState("");
+  // const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState("");
+  const nameRef = useRef();
+  const ageRef = useRef();
 
   const addUserHandler = (e) => {
     e.preventDefault();
-    if(enteredAge.trim().length<=1||enteredName.trim().length<=2){
+    const userName = nameRef.current.value;
+    const userAge = ageRef.current.value;
+    if (userAge.trim().length <= 1 || userName.trim().length <= 2) {
       setError({
-        title:'Name or age is invalid',
-        message:"Please enter valid name (more then 2 alphabet) and age (more then 18)."
-      })
-    
-      return
-    }
-    if(+enteredAge<18){
-      setError({
-        title:'Age is invalid',
-        message:'Please enter valid age (more then 18).'
-      })
-     
+        title: "Name or age is invalid",
+        message:
+          "Please enter valid name (more then 2 alphabet) and age (more then 18).",
+      });
 
-      return
+      return;
     }
-    props.onClickHandle(enteredName,enteredAge)
-    setEnteredAge("");
-    setEnteredName("");
-    
+    if (+userAge < 18) {
+      setError({
+        title: "Age is invalid",
+        message: "Please enter valid age (more then 18).",
+      });
+
+      return;
+    }
+    props.onClickHandle(userName, userAge);
+    nameRef.current.value=''
+    ageRef.current.value=''
+    // setEnteredAge("");
+    // setEnteredName("");
   };
 
   const ageChangeHandler = (e) => {
-    setEnteredAge(e.target.value);
+    // setEnteredAge(e.target.value);
   };
 
   const nameChangeHandler = (e) => {
-    setEnteredName(e.target.value);
+    // setEnteredName(e.target.value);
   };
 
   return (
     <React.Fragment>
-     {error  && <ErrorModal title={error.title} message={error.message} onConfirm={()=>setError(null)} />}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={() => setError(null)}
+        />
+      )}
       <Card style={style.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor='userName'>User Name</label>
           <input
             type='text'
             id='userName'
-            value={enteredName}
-            onChange={nameChangeHandler}
+            // value={enteredName}
+            ref={nameRef}
+            // onChange={nameChangeHandler}
           />
           <label htmlFor='age'>Age (Years)</label>
           <input
             type='text'
             id='age'
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            // value={enteredAge}
+            ref={ageRef}
+            // onChange={ageChangeHandler}
           />
           <Button type='submit'>Add User</Button>
         </form>
